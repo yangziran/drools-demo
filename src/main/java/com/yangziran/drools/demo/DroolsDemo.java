4a2225d1e58f71aa59ea00f87bda21d421b30e25
yangziran <mail@yangziran.com>
2021-11-10 22:07:30 +0800

    first commit

---
+++ src/main/java/com/yangziran/drools/demo/DroolsDemo.java
@@ -0,0 +1,13 @@
+package com.yangziran.drools.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DroolsDemo {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DroolsDemo.class, args);
+    }
+
+}
---
+++ src/test/java/com/yangziran/drools/demo/DroolsDemoTests.java
@@ -0,0 +1,55 @@
+package com.yangziran.drools.demo;
+
+import com.alibaba.fastjson.JSON;
+import com.google.common.collect.Maps;
+import lombok.extern.slf4j.Slf4j;
+import org.drools.core.impl.InternalKnowledgeBase;
+import org.drools.core.impl.KnowledgeBaseFactory;
+import org.junit.jupiter.api.Test;
+import org.kie.api.io.Resource;
+import org.kie.api.io.ResourceType;
+import org.kie.api.runtime.KieSession;
+import org.kie.internal.builder.KnowledgeBuilder;
+import org.kie.internal.builder.KnowledgeBuilderFactory;
+import org.kie.internal.io.ResourceFactory;
+
+import java.io.IOException;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@Slf4j
+class DroolsDemoTests {
+
+    static final String GLOBAL_KEY = "globalResult";
+
+    @Test
+    void contextLoads() throws IOException {
+
+        Resource resource = ResourceFactory.newClassPathResource("test.drl");
+        assertNotNull(resource);
+        log.info("{}", resource.getSourcePath());
+
+        Map<String, Object> paramsMap = Maps.newHashMap();
+        paramsMap.put("amout", 1000);
+        log.info("{}", JSON.toJSONString(paramsMap));
+
+        KnowledgeBuilder kBuilder = KnowledgeBuilderFactory.newKnowledgeBuilder();
+        kBuilder.add(resource, ResourceType.DRL);
+        assertNotNull(kBuilder);
+        InternalKnowledgeBase knowledgeBase = KnowledgeBaseFactory.newKnowledgeBase();
+        knowledgeBase.addPackages(kBuilder.getKnowledgePackages());
+        assertNotNull(knowledgeBase);
+
+        KieSession kieSession = knowledgeBase.newKieSession();
+        kieSession.setGlobal(GLOBAL_KEY, Maps.newHashMap());
+        kieSession.insert(paramsMap);
+        kieSession.fireAllRules();
+        Map<String, Object> resultMap = (Map<String, Object>) kieSession.getGlobal(GLOBAL_KEY);
+        kieSession.dispose();
+
+        log.info("{}", JSON.toJSONString(paramsMap));
+        log.info("{}", JSON.toJSONString(resultMap));
+    }
+
+}
